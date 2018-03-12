24545cb9cfa203292d7f174d3eb8b2a949167c00
Jennifer Kerssen <jjkerssen@gmail.com>
2018-03-11 21:36:18 -0600

    initial commit

---
+++ blogRouter.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+BlogPosts.create('Test Post 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elementum lacus nunc, eu vestibulum mi cursus id.', 'Cicero', 2001);
+BlogPosts.create('Test Post 2', 'Donec pellentesque, purus id tincidunt iaculis, ipsum leo faucibus lectus, vel fermentum libero nunc nec est. Duis euismod lacus vel eros venenatis, eu tristique purus condimentum.', 'Cicero', 2002);
+
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+
+router.post('/', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(item);
+});
+
+
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+  	title: req.params.title, 
+  	content: req.params.content,
+  	author: req.params.author,
+  	publishDate: req.params.publishDate
+  });
+  res.status(204).end();
+})
+
+
+module.exports = router;
